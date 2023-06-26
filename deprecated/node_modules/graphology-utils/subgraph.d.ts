375f330cb4c306e219b42cb9e298decc62024cc6
zayaanali <zayaanali02@gmail.com>
2023-06-26 12:13:38 -0700

    added some stuff

--- deprecated/node_modules/graphology-utils/subgraph.d.ts
+++
@@ -1,7 +0,0 @@
-import Graph, {NodeKey, Attributes} from 'graphology-types';
-
-type SubgraphPredicateFunction = (key: string, attributes: Attributes) => boolean;
-
-type SubgraphNodes = Array<NodeKey> | Set<NodeKey> | SubgraphPredicateFunction;
-
-export default function subgraph(graph: Graph, nodes: SubgraphNodes): Graph;
